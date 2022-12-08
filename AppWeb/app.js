3632da5e0347ead8469918366433be11201ad662
Aeregon-Sharks <camilo07192001@gmail.com>
2022-12-08 00:10:24 -0500

    Prueba callback suma good

--- AppWeb/app.js
+++ AppWeb/app.js
@@ -3,22 +3,34 @@ import express from "express";
 const app = express();
 const port = 3000;
 // Funcion callback para que cuando el servidor empiece a escuchar nos avise
-function callback(){
-    console.log("Server On");
+//function callback(){
+//    console.log("Server On");
+//}
+// Ya no necesitamos la funcion callback ya que usamos una flecha para hacer una funcion rapidamente que no reside en el codigo // ( //parameters ) => { //code }
+function suma(a, b, callback){
+    callback();
+    return a + b;
 }
 
 app.listen(port, () => {
     console.log("Server On");
-})
+} )
 
 app.get("/", (req, res) => {
-    res.send("Hola")
+    res.send("Hola");
 } )
 
 app.get("/home", (req, res) => {
-    res.send("Menu Home")
+    res.send("Menu Home");
 } )
 
 app.get("/home/login", (req, res) => {
-    res.send("Menu Login")
+    res.send("Menu Login");
+} )
+
+app.get("/home/suma", (req, res) => {
+    res.send("sumanding")
+    console.log(suma(10, 20, ()=>{
+        console.log("Sumanding",10,"+",20,"...")
+    }));
 } )
